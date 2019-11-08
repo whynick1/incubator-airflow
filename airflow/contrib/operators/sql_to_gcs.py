e0bacdac70c36e2300302ff8779b97c1953491a6
Yiming Liu <yimingl@wepay.com>
2019-11-08 18:19:35 +0000

    Add sql_to_gcs schema
    Squashed commit of the following:
    
    commit a2ff00eb3aa3f2c3d20ee2d42bc5ae7892e9b20e
    Author: Yiming Liu <yimingl@wepay.com>
    Date:   Fri Nov 8 10:09:27 2019 -0800
    
        minor
    
    commit 4d33d3b77547075eb39955328c2d13dca588f679
    Author: Yiming Liu <yimingl@wepay.com>
    Date:   Fri Nov 8 10:05:03 2019 -0800
    
        manually cherry pick from AIRFLOW-2169:Add schema to MySqlToGoogleCloudStorageOperator

--- airflow/contrib/operators/sql_to_gcs.py
+++ airflow/contrib/operators/sql_to_gcs.py
@@ -241,26 +241,49 @@ class BaseSQLToGoogleCloudStorageOperator(BaseOperator):
         """
         Takes a cursor, and writes the BigQuery schema for the results to a
         local file system.
-
+        
         :return: A dictionary where key is a filename to be used as an object
             name in GCS, and values are file handles to local files that
             contains the BigQuery schema fields in .json format.
         """
         schema = [self.field_to_bigquery(field) for field in cursor.description]
+        tmp_schema_file_handle = NamedTemporaryFile(delete=True)
+        if self.schema is not None and isinstance(self.schema, string_types):
+            schema = self.schema
+            tmp_schema_file_handle.write(schema)
+        else:
+            if self.schema is not None and isinstance(self.schema, list):
+                schema = self.schema
+            else:
+                for field in cursor.description:
+                    # See PEP 249 for details about the description tuple.
+                    field_name = field[0]
+                    field_type = self.type_map(field[1])
+                    # Always allow TIMESTAMP to be nullable. MySQLdb returns None types
+                    # for required fields because some MySQL timestamps can't be
+                    # represented by Python's datetime (e.g. 0000-00-00 00:00:00).
+                    if field[6] or field_type == 'TIMESTAMP':
+                        field_mode = 'NULLABLE'
+                    else:
+                        field_mode = 'REQUIRED'
+                    schema.append({
+                        'name': field_name,
+                        'type': field_type,
+                        'mode': field_mode,
+                    })
+            s = json.dumps(schema, tmp_schema_file_handle)
+            if PY3:
+                s = s.encode('utf-8')
+            tmp_schema_file_handle.write(s)
 
         self.log.info('Using schema for %s: %s', self.schema_filename, schema)
-        tmp_schema_file_handle = NamedTemporaryFile(delete=True)
-        schema_str = json.dumps(schema, sort_keys=True)
-        if PY3:
-            schema_str = schema_str.encode('utf-8')
-        tmp_schema_file_handle.write(schema_str)
-        schema_file_to_upload = {
+	    schema_file_to_upload = {
             'file_name': self.schema_filename,
             'file_handle': tmp_schema_file_handle,
             'file_mime_type': 'application/json',
         }
         return schema_file_to_upload
-
+    
     def _upload_to_gcs(self, files_to_upload):
         """
         Upload all of the file splits (and optionally the schema .json file) to
